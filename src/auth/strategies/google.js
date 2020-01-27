5257dd4fde496e1e5b5b56201d49382ca80aab17
Justin Yum <justinyum98@gmail.com>
2020-01-26 20:53:16 -0800

    use lodash for performance

--- src/auth/strategies/google.js
+++ src/auth/strategies/google.js
@@ -1,5 +1,6 @@
 const { OAuth2Strategy } = require('passport-google-oauth');
 const { findOrCreateUser } = require('../../database/dataAccess/user');
+const _ = require('lodash');
 
 const createStrategy = (passport) => (
   new OAuth2Strategy({
@@ -7,9 +8,9 @@ const createStrategy = (passport) => (
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK_URL
   }, (accessToken, refreshToken, profile, done) => {
-    const email = profile.emails[0].value;
-    const givenName = profile.name.givenName;
-    const familyName = profile.name.familyName;
+    const email = _.get(profile, 'emails[0].value');
+    const givenName = _.get(profile, 'name.givenName');
+    const familyName = _.get(profile, 'name.familyName');
     findOrCreateUser({ email, givenName, familyName })
       .then((user) => done(null, user))
       .catch((err) => done(err));
