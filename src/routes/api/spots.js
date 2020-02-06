46b17a9dcd5e4eefc091daf465fab31b14bc3b33
Justin Yum <justinyum98@gmail.com>
2020-02-05 16:36:43 -0800

    fixed up Spot structure

--- src/routes/api/spots.js
+++ src/routes/api/spots.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 // Todo Model
-const Spot = require("../../models/Spots");
+const Spot = require('../../database/models/spot');
 
 // @route   GET api/todos
 // @desc    Get All Todos
